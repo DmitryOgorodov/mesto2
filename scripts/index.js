d74310162f9e16c492897f258b5eb4c5717f837c
Dmitry Ogorodov <dmitry.ogorodov@yandex.ru>
2023-09-05 03:06:25 +0600

    Реализация функционала формы добавления карточки изображений

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,34 @@
-const form = document.querySelector('.form');
+
+/**
+ * *******************  Форма редактирования профиля formUserInfo   ******************
+ * 
+ * функции:
+ * - formEditHandler
+ * - formSubmitHandler
+ */
+
+const formUserInfo = document.querySelector('#formUserInfo');
+
+const popupUserInfo = document.querySelector('#popupUserInfo');
+
 const editButton = document.querySelector('.button__edit-profile');
 const closeButton = document.querySelector('.button__close');
-const saveButton = document.querySelector('.button__submit');
-const popup = document.querySelector('.popup');
+//const saveButton = document.querySelector('.button__submit');
+
 const nameInput = document.querySelector('#name-field');
 const jobInput = document.querySelector('#caption-field');
 
-function toglePopup () {
+function toglePopup (popup) {
     popup.classList.toggle('popup_opened');
 }
 
 const profileName = document.querySelector('.profile__title');// Выберите элементы, куда должны быть вставлены значения полей
 const profileJob = document.querySelector('.profile__caption');
 
-function formEditHandler (evt) {
+function formEditHandler () {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-    toglePopup();
+    toglePopup(popupUserInfo);
 }
 
 function formSubmitHandler (evt) {
@@ -28,13 +40,26 @@ function formSubmitHandler (evt) {
     
     profileName.textContent = nameInput.value;// Вставьте новые значения с помощью textContent
     profileJob.textContent = jobInput.value;
+    toglePopup(popupUserInfo);
 }
 
-
-form.addEventListener('submit', formSubmitHandler); 
-
 editButton.addEventListener('click', formEditHandler);
-closeButton.addEventListener('click', toglePopup);
-saveButton.addEventListener('click', toglePopup);
+closeButton.addEventListener('click', () => toglePopup(popupUserInfo));
+//saveButton.addEventListener('click', toglePopup);
+
+formUserInfo.addEventListener('submit', formSubmitHandler); 
 
 
+/**
+ * *******************  Форма добавления карточки formAddImage  *******************
+ */
+
+const formAddImage = document.querySelector('#formAddImage')
+
+const popupAddImage = document.querySelector('#popupAddImage')
+
+const buttonAddImage = document.querySelector('.button__add-image')
+const buttonResetCard = document.querySelector('#resetCard') 
+
+buttonAddImage.addEventListener('click', () => toglePopup(popupAddImage))
+buttonResetCard.addEventListener('click', () => toglePopup(popupAddImage))
